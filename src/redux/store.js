de1537bd3c0ae333fb4baa84be9613f1e8b304dc
Pmzkh078 <patelpratikh98@gmail.com>
2024-02-12 13:24:50 +0530

    portfolio

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { InputMessage } from "./slices/messageSlice";
-
-export const store = configureStore({
-  reducer: {
-    Message: InputMessage.reducer,
-  },
-});
